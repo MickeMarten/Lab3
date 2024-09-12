a0aaeb6c789c9ca13aff7c83fb235a1e52fbe588
Felix Hultman <karlfelixhultman@gmail.com>
2024-09-12 15:53:42 +0200

    efter att ha kraschat förra commiten ska det här nog rädda det vi har gjort. Co-authored-by: KarlMikael.Martensson@gmail.com

--- src/main/java/service/Warehouse.java
+++ src/main/java/service/Warehouse.java
@@ -1,7 +1,10 @@
 package service;
+
 import entities.Category;
 import entities.Product;
+
 import java.time.LocalDateTime;
+import java.time.chrono.ChronoLocalDate;
 import java.util.ArrayList;
 import java.time.LocalDate;
 import java.util.Collections;
@@ -12,38 +15,35 @@ import java.util.stream.Collectors;
 public class Warehouse {
     private static final List<Product> products = new ArrayList<>();
 
+    public static void resetProducts() {
+        products.clear();
+    }
+
+
     public static void main(String[] args) {
-        addProduct();
-        List<Product> test = getProductByCategorySortedByName(Category.DRINK);
+        addProductForTest();
+
+        List<Product> test = getProductsThatHasBeenModified();
         System.out.println(test);
     }
 
-    public static void addProduct(){
-        products.add(new Product(1, "Apelsin", Category.FRUITS,5, LocalDate.of(2024, 12, 30), LocalDate.of(2024, 1,28)));
-        products.add(new Product(2, "Banan", Category.FRUITS, 1, LocalDate.of(2023, 8, 25), LocalDate.of(2024, 2,23)));
-        products.add(new Product(3, "Kyckling", Category.MEAT, 9, LocalDate.of(2024, 11, 22), LocalDate.of(2024, 3,11)));
-        products.add(new Product(5, "Pepsi", Category.DRINK, 6, LocalDate.of(2023, 6, 12), LocalDate.of(2024, 9,2)));
-        products.add(new Product(6, "Fanta", Category.DRINK, 1, LocalDate.of(2023, 6, 12), LocalDate.of(2024, 7,5)));
-        products.add(new Product(7, "Zola", Category.DRINK, 10, LocalDate.of(2023, 6, 12), LocalDate.of(2024, 6,2)));
-        products.add(new Product(8, "Fläsk", Category.MEAT,8, LocalDate.of(2023, 4, 4), LocalDate.of(2024, 4,4)));
+    public static void addProductForTest() {
+        products.add(new Product(1, "Apelsin", Category.FRUITS, 5, LocalDate.of(2024, 12, 30),null));
+        products.add(new Product(2, "Banan", Category.FRUITS, 1, LocalDate.of(2023, 8, 25), LocalDate.of(2024, 2, 23)));
+        products.add(new Product(3, "Kyckling", Category.MEAT, 9, LocalDate.of(2024, 11, 22), LocalDate.of(2024, 3, 11)));
+        products.add(new Product(5, "Pepsi", Category.DRINK, 6, LocalDate.of(2023, 6, 12), LocalDate.of(2024, 9, 2)));
+        products.add(new Product(7, "Zola", Category.DRINK, 10, LocalDate.of(2023, 6, 12), LocalDate.of(2024, 6, 2)));
+        products.add(new Product(6, "Fanta", Category.DRINK, 1, LocalDate.of(2023, 6, 12), LocalDate.of(2023, 8, 12)));
+        products.add(new Product(8, "Fläsk", Category.MEAT, 8, LocalDate.of(2023, 4, 4), LocalDate.of(2024, 4, 4)));
     }
 
 
-    public static List<Product> getAllProducts(){
+    public static List<Product> getAllProducts() {
+
+
         return Collections.unmodifiableList(products);
     }
 
-
-
-    public static void sortProduct(){
-        List<Product> sortedList = products.stream()
-                .filter(item -> item.rating() > 1 ).peek(System.out::println)
-                .collect(Collectors.toList());
-
-        System.out.println(sortedList);
-
-
-    }
 
     public static Product getProductById(int id) {
 
@@ -53,19 +53,44 @@ public class Warehouse {
     }
 
     public static List<Product> getProductByCategorySortedByName(Category category) {
-        return products.stream().filter(product -> product.category() == category).sorted(Comparator.comparing(Product::name)).collect(Collectors.toUnmodifiableList());
-        
+        return products.stream()
+                .filter(product -> product.category() == category)
+                .sorted(Comparator.comparing(Product::name))
+                .collect(Collectors.toUnmodifiableList());
+
     }
 
+    public static void addNewProduct(Product product) {
+        if (product.name().isEmpty()) {
+            throw new IllegalArgumentException("Product name can't be empty");
+        }
+        products.add(product);
 
+    }
 
+    public static void modifyProduct(int id, String newName, Category newCategory, int newRating) {
+        Product product = products.stream()
+                .filter(p -> p.id() == id).findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("Id is not available"));
+        Product modifiedProduct = new Product(product.id(), newName, newCategory, newRating, product.createdAt(), LocalDate.now());
 
-    //Lägga till en ny produkt (Bör ha någon enkel valdiering så prdukter utan namn ej kan läggas till.
-    // Modifeiera en befintlig produkt (Byta namn, kategori, rating).
-    // Hämta alla produkter skapde efter angivet Datum. (Nya produkter sist).
-    // Hämta alla produkter  som modiferats sen de skapades (Datum ej samma).
-    // Klara.
-    // Hämta alla produkter.
-    // Hämta en produkt genom produktens id.
+        products.set(products.indexOf(product), modifiedProduct);
+
+    }
+
+    public static List<Product> getProductsAfterDate(LocalDate dateAfter) {
+        return products.stream().filter(product -> product.createdAt().isAfter(dateAfter)).collect(Collectors.toUnmodifiableList());
+
+    }
+
+    public static List<Product> getProductsThatHasBeenModified() {
+        return products.stream()
+                .filter(product -> product.modifiedAt() != null && !product.createdAt()
+                        .isEqual(product.modifiedAt()))
+                .collect(Collectors.toUnmodifiableList());
+    }
+    //Todo: Lägg till test för getproductsThathasBeenmodified
+    //Todo: Lägg till Test för addNewProduct
+
 
 }
--- src/test/java/service/WarehouseTest.java
+++ src/test/java/service/WarehouseTest.java
@@ -1,9 +1,14 @@
 package service;
 
+import entities.Category;
 import entities.Product;
+import jdk.jfr.Name;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -14,25 +19,82 @@ public class WarehouseTest {
     private Warehouse warehouse;
     @BeforeEach
     void setUp() {
+        Warehouse.resetProducts();
+        System.out.println("Products before test: " + Warehouse.getAllProducts());
         warehouse = new Warehouse();
-        warehouse.addProduct();
-   }
-
+        warehouse.addProductForTest();
+    }
+    @AfterEach
+    void tearDown() {
+        System.out.println("Products after test: " + Warehouse.getAllProducts());
+    }
 
     @Test
+    @DisplayName("Test to get all products")
     void testToGetAllProductsInList() {
-        List<Product> result = Warehouse.getAllProducts();
+        List<Product> result = warehouse.getAllProducts();
 
-        assertNotNull(result, "List should not be null");
+        assertNotNull(result, "ProductList should not be empty.");
 
-       assertTrue(result.size() > 0, "List should not be empty");
+        assertTrue(result.size() > 0, "");
     }
+
 
     @Test
+    @DisplayName("Test to get a product by id")
     void getProductById() {
-        Product result = Warehouse.getProductById(1);
+        Product result = warehouse.getProductById(1);
         assertEquals(1, result.id(), "Wrong id");
+    }
+
+
+    @Test
+    @DisplayName("Test to get products by category and sorted by name")
+    void getProductByCategorySortedByName() {
+        List<Product> result = warehouse.getProductByCategorySortedByName(Category.DRINK);
+        assertFalse(result.isEmpty(), "List with drinks should not be empty.");
+        assertEquals("Zola", result.getLast().name());
+        for (Product product : result) {
+            assertEquals(Category.DRINK, product.category());
+        }
+    }
+
+
+    @Test
+    @DisplayName("Test to modify a product")
+    void testModifyProductSuccessfully() {
+
+        warehouse.modifyProduct(1, "Pepsi", Category.DRINK, 9);
+
+        Product modifiedProduct = warehouse.getProductById(1);
+        assertEquals(1, modifiedProduct.id());
+        assertEquals("Pepsi", modifiedProduct.name());
+        assertEquals(Category.DRINK, modifiedProduct.category());
+        assertEquals(9, modifiedProduct.rating());
 
 
     }
+
+
+    @Test
+    @DisplayName("??")
+    void testModifyNonExistentProductThrowsException() {
+
+        assertThrows(IllegalArgumentException.class, () -> warehouse.modifyProduct(999, "Pepsi", Category.DRINK, 9), "Product found");
+    }
+
+
+    @Test
+    @DisplayName("Test to get a product after given date")
+    void testGetProductAfterGivenDate() {
+        LocalDate dateTest = LocalDate.of(2024,11,21);
+        List<Product> result = warehouse.getProductsAfterDate(dateTest);
+        assertEquals(2, result.size());
+        for(Product product : result){
+            assertTrue(product.createdAt().isAfter(dateTest));
+        }
+
+
+    }
+
 }
